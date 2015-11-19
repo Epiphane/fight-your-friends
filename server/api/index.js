0200a830665780fb626ec2744d663fde85378ea9
Thomas Steinke <exyphnos@gmail.com>
2015-11-19 08:03:08 -0800

    Slack endpoint

--- server/api/index.js
+++ server/api/index.js
@@ -1,8 +1,17 @@
-var express = require('express');
-var app     = new express();
-var Engine  = require('../../engine');
+var express    = require('express');
+var app        = new express();
+var Engine     = require('../../engine');
+var bodyParser = require('body-parser');
 
 module.exports = app;
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+//   extended: true
+// })); 
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get('/', function(req, res) {
    res.end('Hello!');
@@ -14,7 +23,12 @@ app.get('/lookup', function(req, res) {
    if (!req.query.name) res.end('[]');
    else {
       Engine.lookup(req.query.name).then(function(result) {
-         res.end(JSON.stringify(result));
+         res.json(result).end();
       });
    }
+});
+
+app.post('/slack', function(req, res) {
+   console.log(req.body);
+   res.json(req.query).end();
 });
